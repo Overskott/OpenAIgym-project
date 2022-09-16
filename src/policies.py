b56a4c38974b6e52d12cd34ed153194d28694bfa
Sebastian T. Overskott <overskotts@gmail.com>
2022-09-16 14:56:28 +0200

    Added simple_ca ploicy

--- src/policies.py
+++ src/policies.py
@@ -1,3 +1,7 @@
+import CA
+
+def right_control(observation):
+    return 1
 
 
 def random_control(observation):
@@ -9,6 +13,25 @@ def random_control(observation):
 
 def naive_control(observation):
     if observation[2] >= 0:
+        return 1
+    else:
+        return 0
+
+
+def simple_ca(observation, rule):
+    state = CA.observables_to_binary(observation)[1:]
+    action = CA.increment_state(state, rule)
+
+    return action
+
+
+def voter_control(observation):
+    state = CA.observables_to_binary(observation)
+    sum = 0
+    for i in state:
+        sum += int(i)
+    print(sum)
+    if sum/4 < 1/2:
         return 1
     else:
         return 0
