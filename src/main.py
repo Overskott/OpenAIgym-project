a80d6b273ecfee22dc45e258f321a99c61b59aa0
Sebastian T. Overskott <overskotts@gmail.com>
2022-09-16 14:56:53 +0200

    Now searches all rules for best solution

--- src/main.py
+++ src/main.py
@@ -3,26 +3,40 @@
 #
 #
 import gym
-import time
 import policies
 
-env = gym.make("CartPole-v1", render_mode="human")
-observation, info = env.reset(seed=42)
+env = gym.make("CartPole-v1")
 
-print(['Position', 'Velocity', 'Angle', 'Ang. vel.'])
-score = 0
-for _ in range(50):
-    action = policies.naive_control(observation)  # User-defined policy function
-    observation, reward, terminated, truncated, info = env.step(action)
-    score += reward
-    print(f"reward: {reward}, terminated: {terminated}, turncated: {truncated}, info: {info}")
-    print(observation)
-    if terminated:
-        observation, info = env.reset()
-        print(f"Run ended  with score {score}")
-        break
-    if terminated:
-        observation, info = env.reset()
-    time.sleep(0.05)
+run = 0
+results = [0, 0, 0]
 
-env.close()
+for run in range(258):
+    score = 0
+    rule = run
+    observation, _ = env.reset(seed=546)
+
+    for _ in range(500):
+        action = policies.simple_ca(observation, rule)  # User-defined policy function
+        observation, reward, terminated, truncated, _ = env.step(action)
+
+        score += reward
+
+        if terminated:
+            observation = env.reset()
+            print(f"Run terminated with score {score}")
+            break
+        elif truncated:
+            observation = env.reset()
+            print(f"Run truncated with score {score}")
+            break
+
+    env.close()
+
+    print(f"Run {run}, score {score}, rule {rule}")
+
+    if score > results[1]:
+        results[0] = run
+        results[1] = score
+        results[2] = rule
+
+print(f"Best run was run #{results[0]} with the score {results[1]} using rule {results[2]}")
