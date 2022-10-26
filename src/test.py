173fcdeccf2c6c72ab2e0c6694e2754ecba16232
Sebastian T. Overskott <overskotts@gmail.com>
2022-10-26 11:09:55 +0200

    Created nn_environment.py

---
+++ src/test.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from evolution import *
+
+A = np.random.random((4, 4))
+
+B = np.random.random((4, 5))
+
+print(B)
+print(A)
+
+nn_crossover(A, B)
+#
+#
+#
+print(A)
+print(B)
+
+
+
+
